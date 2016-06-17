f603b966c779794541b80a1677e155cc881a84a2
Alan Palomero <alan.palomero@gmail.com>
2016-06-17 03:40:45 -0300

    removido CDn de todos os arquivos

--- alterar_tag.php
+++ alterar_tag.php
@@ -20,14 +20,16 @@
 	<title>Jinteki - Alterar</title>
 	<meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  <script src="center.js"></script>
-  <link rel="stylesheet" type="text/css" href="signin.css" />
+  <link rel="stylesheet" href="estilos/w3.css">
+  <link rel="stylesheet" href="estilos/bootstrap.min.css" />
+  <script src="scripts/jquery.min.js"></script>
+  <script src="scripts/bootstrap.min.js"></script>
+  <script src="scripts/center.js"></script>
+  <script src="scripts/jquery.maskedinput.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="estilos/signin.css" />
   <link rel="stylesheet" href="estilos/movimentacao.css" />
   <link rel="stylesheet" href="estilos/detalhes.css" />
+  <link rel="stylesheet" href="estilos/pendencias.css" /> 
   
 </head>
 
